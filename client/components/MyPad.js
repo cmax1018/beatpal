5212315650fd438c3ac99c45b4ad64c1bd6e9e5b
Max Van Buskirk <cmax1018@gmail.com>
2020-03-12 19:08:05 -0400

    pads now play in tandem correctly. selected keys no longer light up

--- client/components/MyPad.js
+++ client/components/MyPad.js
@@ -5,34 +5,71 @@ class MyPad extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      keyPressed: ''
+      keysPressed: []
     }
   }
   async componentDidMount() {
     console.log('starting Tone...')
     await Tone.start()
+    await Tone.Transport.start()
+    Tone.context.lookAhead = 0
+
     this.handleDown = this.handleDown.bind(this)
+    this.handleUp = this.handleUp.bind(this)
   }
-  handleDown = e => {
-    console.log(e.key, ' pressed')
-    this.setState({
-      keyPressed: e.key
-    })
+  handleDown = async e => {
+    const keyPressed = e.key
+    if (!this.state.keysPressed.includes(keyPressed)) {
+      await this.setState(state => {
+        const keysPressed = [
+          ...state.keysPressed.filter(key => !state.keysPressed.includes(key)),
+          keyPressed
+        ]
+        return {
+          keysPressed
+        }
+      })
+    }
   }
+  handleUp = async e => {
+    const keyPressed = e.key
+    if (this.state.keysPressed.includes(keyPressed)) {
+      await this.setState(state => {
+        const keysPressed = state.keysPressed.filter(key => key !== keyPressed)
+        return {
+          keysPressed
+        }
+      })
+    }
+  }
+
   render() {
     return (
-      <div onKeyDown={this.handleDown} tabIndex="0">
+      <div
+        onKeyDown={this.handleDown}
+        onKeyUp={this.handleUp}
+        tabIndex="0"
+        className="focus-window"
+      >
         <h1>mypad</h1>
-        <Pad
-          url="assets/snare.mp3"
-          keyCode="s"
-          keyPressed={this.state.keyPressed}
-        />
-        <Pad
-          url="assets/kick.mp3"
-          keyCode="a"
-          keyPressed={this.state.keyPressed}
-        />
+        <div className="padContainer">
+          <Pad
+            url="assets/kick.mp3"
+            keyCode="a"
+            keysPressed={this.state.keysPressed}
+          />
+          <Pad
+            url="assets/snare.mp3"
+            keyCode="s"
+            keysPressed={this.state.keysPressed}
+            className="pad"
+          />
+          <Pad
+            url="assets/crash.mp3"
+            keyCode="d"
+            keysPressed={this.state.keysPressed}
+          />
+        </div>
       </div>
     )
   }
--- client/components/Pad.js
+++ client/components/Pad.js
@@ -2,27 +2,35 @@ import React from 'react'
 import * as Tone from 'tone'
 
 class Pad extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playing: false
+    }
+    this.play = this.play.bind(this)
+  }
   async componentDidMount() {
     this.player = await new Tone.Player(this.props.url).toMaster()
-    this.loaded = true
-    console.log('sampled loaded')
+    console.log(this.player.buffer)
   }
-  async componentDidUpdate() {
-    if (this.loaded && this.props.keyCode === this.props.keyPressed) {
-      await this.player.start(null, null, 1)
+  componentDidUpdate() {
+    if (this.props.keysPressed.includes(this.props.keyCode)) {
+      this.play()
+    }
+  }
+
+  play() {
+    if (this.state.playing) {
+      this.player.restart()
+    } else {
+      return this.player.start()
     }
   }
 
   render() {
     return (
-      <div onKeyDown={this.handleDown}>
-        <h2
-          className={
-            this.props.keyCode === this.props.keyPressed ? 'selected' : ''
-          }
-        >
-          PAD
-        </h2>
+      <div className={this.state.playing ? 'pad selected' : 'pad'}>
+        {this.props.keyCode}
       </div>
     )
   }
